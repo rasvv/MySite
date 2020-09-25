ab6e85afe64bf4d0fb24ec26232937eb1069a855
rasvv <rasvv@mail.ru>
2020-09-25 12:22:57 +0300

    Перед переводом на grid

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Личный сайт студента GeekBrains</title>
     <link rel="shortcut icon" href="img/Icon.png" type="image/png">
     <link rel="stylesheet" href="css/reset.css">
