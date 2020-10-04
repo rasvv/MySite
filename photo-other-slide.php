7cc3768e1ba133776b556aeb8db40b09dd8c740e
rasvv <rasvv@mail.ru>
2020-10-04 20:43:57 +0300

    Перевел на слайд-шоу

---
+++ photo-other-slide.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <?php
+		include "head.php"
+	?>
+<body>
+ 	<?php
+		include "menu.php"
+    ?>   
+
+    <section class="slider-top">
+        <div class="slider">
+            <div class="slider__item" style="background-image: url(img/Other/Full/1.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/2.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/3.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/4.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/5.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/6.jpg);"></div>
+            <div class="slider__item" style="background-image: url(img/Other/Full/7.jpg);"></div>
+        </div>
+    </section>
+
+    <?php
+		include "footer.php"
+  ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/slick.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
