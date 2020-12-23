f6e13adc049bbb7cc56b9aa3ae9f709532a8522d
rasvv <rasvv@mail.ru>
2020-12-23 23:11:38 +0300

    Сделал слайдер, галерею и программы

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,14 +1,15 @@
 $(function () {
 	$('.slider').slick({
-		//fade: true,
-		// arrows: true,
+		fade: true,
+		arrows: true,
 		//autoplay: false,
 		//adaptiveHeight: true,
-		//dots: true,
+		dots: true,
 		//infinite: true//,
 		// speed: 300,
 		// slidesToShow: 1//,
 		// adaptiveHeight: true
+		centrmode: true
 	});
 });
 
@@ -21,15 +22,30 @@ $(function () {
 // 	})
 
 
-// $(function () {
-// 	$('.gallery__item').on('click', function () {
-// 		$('.gallery__item').addClass('active');
-// 	})
+$(function () {
+	$('.tab__guess').on('click', function () {
+		// $('.tabs__puzzle').removeClass('active');
+		// $('.tabs__guess').removeClass('active');
+		$('.tabs__item').removeClass('active');
+		$('.tab__guess').addClass('active');
+	})
+	$('.tab__puzzle').on('click', function () {
+		// $('.tabs__puzzle').removeClass('active');
+		// $('.tabs__guess').removeClass('active');
+		$('.tabs__item').removeClass('active');
+		$('.tab__puzzle').addClass('active');
+	})
+	$('.tab__password').on('click', function () {
+		// $('.tabs__puzzle').removeClass('active');
+		// $('.tabs__guess').removeClass('active');
+		$('.tabs__item').removeClass('active');
+		$('.tab__password').addClass('active');
+	})
 
-// 	$('.gallery__item').on('click', function () {
-// 		$('.gallery__item').removeClass('active');
-// 	})
-// });
+	// $('.tabs__block').on('click', function () {
+	// 	$('.tabs__block').removeClass('active');
+	// })
+});
 
 // $(function () {
 // 	$('.minimized').on('click', function (event) {
